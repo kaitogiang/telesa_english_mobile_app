21e198e3e664baaf2bb9c99db9e51865502233bf
Giang Dai Huy <huygiang001442@gmail.com>
2025-09-20 09:29:57 +0700

    chore: add init dependency and init project

--- lib/core/config/di.config.dart
+++ lib/core/config/di.config.dart
@@ -13,21 +13,8 @@ import 'package:firebase_auth/firebase_auth.dart' as _i59;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:google_sign_in/google_sign_in.dart' as _i116;
 import 'package:injectable/injectable.dart' as _i526;
-import 'package:soundx/core/config/external_injectable.dart' as _i637;
-import 'package:soundx/features/auth/data/datasources/auth_remote_data_source.dart'
-    as _i214;
-import 'package:soundx/features/auth/data/repsitories_impl/remote_auth_repositories_impl.dart'
-    as _i213;
-import 'package:soundx/features/auth/domain/repositories/remote_auth_repositories.dart'
-    as _i267;
-import 'package:soundx/features/auth/domain/usecases/reset_password_usecase.dart'
-    as _i792;
-import 'package:soundx/features/auth/domain/usecases/sign_in_with_google_usecase.dart'
-    as _i809;
-import 'package:soundx/features/auth/domain/usecases/sign_out_usecase.dart'
-    as _i199;
-import 'package:soundx/features/auth/domain/usecases/sign_up_usecase.dart'
-    as _i824;
+import 'package:telesa_english_app/core/config/external_injectable.dart'
+    as _i327;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -39,29 +26,8 @@ extension GetItInjectableX on _i174.GetIt {
     final externalInjectable = _$ExternalInjectable();
     gh.lazySingleton<_i59.FirebaseAuth>(() => externalInjectable.firebaseAuth);
     gh.lazySingleton<_i116.GoogleSignIn>(() => externalInjectable.googleSigIn);
-    gh.lazySingleton<_i214.AuthRemoteDataSource>(
-      () => _i214.AuthRemoteDataSource(
-        gh<_i59.FirebaseAuth>(),
-        gh<_i116.GoogleSignIn>(),
-      ),
-    );
-    gh.lazySingleton<_i267.RemoteAuthRepositories>(
-      () => _i213.RemoteAuthRepositoriesImpl(gh<_i214.AuthRemoteDataSource>()),
-    );
-    gh.lazySingleton<_i792.ResetPasswordUseCase>(
-      () => _i792.ResetPasswordUseCase(gh<_i267.RemoteAuthRepositories>()),
-    );
-    gh.lazySingleton<_i199.SignOutUseCase>(
-      () => _i199.SignOutUseCase(gh<_i267.RemoteAuthRepositories>()),
-    );
-    gh.lazySingleton<_i824.SignUpUseCase>(
-      () => _i824.SignUpUseCase(gh<_i267.RemoteAuthRepositories>()),
-    );
-    gh.lazySingleton<_i809.SignInWithGoogleUseCase>(
-      () => _i809.SignInWithGoogleUseCase(gh<_i267.RemoteAuthRepositories>()),
-    );
     return this;
   }
 }
 
-class _$ExternalInjectable extends _i637.ExternalInjectable {}
+class _$ExternalInjectable extends _i327.ExternalInjectable {}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,34 @@
 import FlutterMacOS
 import Foundation
 
+import audio_service
+import audio_session
+import cloud_firestore
+import firebase_auth
+import firebase_core
+import firebase_database
+import firebase_storage
+import flutter_localization
+import google_sign_in_ios
+import just_audio
+import path_provider_foundation
+import shared_preferences_foundation
+import sqflite_darwin
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
