b991e9d54bd8e3ffc36d4513fcb2386104a4783d
Giang Dai Huy <huygiang001442@gmail.com>
2025-09-21 18:25:41 +0700

    feat: create English for kids secion

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:telesa_english_app/core/constants/app_color.dart';
 import 'package:telesa_english_app/features/home/presentation/widgets/app_container.dart';
+import 'package:telesa_english_app/features/home/presentation/widgets/category_section.dart';
 import 'package:telesa_english_app/features/home/presentation/widgets/course_progress_card.dart';
 import 'package:telesa_english_app/features/home/presentation/widgets/home_header.dart';
+import 'package:telesa_english_app/features/home/presentation/widgets/lesson_card.dart';
 import 'package:telesa_english_app/features/home/presentation/widgets/quiz_card.dart';
 import 'package:telesa_english_app/features/home/presentation/widgets/streak_card.dart';
 import 'package:telesa_english_app/features/shared/presentation/widget/discount_banner.dart';
@@ -46,6 +48,19 @@ class Home extends StatelessWidget {
                   title: 'Listening & Pronunciation',
                   questionOnAction: () {},
                   timeOnAction: () {},
+                ),
+                AppSizes.s20.verticalGap,
+                CategorySection(
+                  headerTitle: 'English for Kids',
+                  onAction: () {},
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: List<Widget>.generate(4, (index) {
+                        return LessonCard(isShowLevelChip: false);
+                      }),
+                    ),
+                  ),
                 ),
               ],
             ),
